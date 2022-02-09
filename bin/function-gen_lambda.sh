6967668fd5c88a8dce9a1c38906b19eee9d46f42
Abir Ganguly <abir@grappa.iqb.rutgers.edu>
2022-02-08 22:11:16 -0500

    gen_lambda function py2.7 --> py3

--- bin/function-gen_lambda.sh
+++ bin/function-gen_lambda.sh
@@ -2,20 +2,21 @@
 # generate equally spaced lambda windows
 function gen_lambdas {
         cat <<EOF > gen_lambda.py
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import numpy as np
 import sys
 
-nlam = sys.argv[1]; nlam = float (nlam)
-a = np.linspace(0, 1, num = nlam, dtype = float)
+nlam = sys.argv[1]; nlam = int (nlam)
+#nlam = sys.argv[1]; nlam = float (nlam)
+a = np.linspace(0, 1, num = nlam)
 for x in range(len(a)):
     print("{:.8f}".format(a[x])),
 
 EOF
 
 local nlambda=$1
-local lams=$(python2.7 gen_lambda.py ${nlambda})
+local lams=$(python3 gen_lambda.py ${nlambda})
 echo $lams
 rm -rf gen_lambda.py
 }
